0429b902c65edb68f213667212ee7b0a9de9dbd8
Alicia Rufino <a.rufino@altraduct.com>
2015-06-19 15:40:19 +0200

    Exercises The Basics

---
+++ 1_Intro_The Basics.rb
@@ -0,0 +1,52 @@
+#Exercise 1 - The Basics
+
+puts "Alicia " + "Rufino"
+
+#Exercise 2 - The Basics
+
+a = 1973
+puts a / 1000
+puts a % 1000 / 100
+puts a % 100 / 10
+puts a % 10
+
+#Exercise 3 - The Basics
+
+movies = { 
+  :Movie_1 => 1975,
+  :Movie_2 => 2004,
+  :Movie_3 => 2013,
+  :Movie_4 => 2001,
+  :Movie_5 => 1981 
+}
+	
+puts movies[ :Movie_1 ]
+puts movies[ :Movie_2 ]
+puts movies[ :Movie_3 ]
+puts movies[ :Movie_4 ]
+puts movies[ :Movie_5 ]
+
+#Exercise 4 - The Basics
+
+movies = [1975, 2004, 2013, 2001, 1981]
+puts movies[0]
+puts movies[1]
+puts movies[2]
+puts movies[3]
+puts movies[4]
+
+#Exercise 5 - The Basics
+
+puts 1 * 2 * 3 * 4 * 5
+puts 1 * 2 * 3 * 4 * 5 * 6
+puts 1 * 2 * 3 * 4 * 5 * 6 * 7
+puts 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8
+
+#Exercise 6 - The Basics
+
+puts 3.2 * 3.2
+puts 5.5 * 5.5
+puts 3.3 * 3.3
+
+#Exercise 7 - The Basics
+#There is a closing bracket instead of a }. 
