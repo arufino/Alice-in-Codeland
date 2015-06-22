1738581e0c55206db788d0653f86ed01cac82295
Alicia Rufino <a.rufino@altraduct.com>
2015-06-22 15:49:05 +0200

    methods exercises

---
+++ 3_Intro_Methods.rb
@@ -0,0 +1,38 @@
+##Exercise 1 - Methods
+
+def greeting(name)
+	"Welcome to our site, " + name
+end
+
+puts greeting("Antonio")
+
+#Exercise 2 - Variables
+#1. x = 2 => 2
+#2. puts x = 2 => nil
+#3. p name = "Joe" => "Joe"
+#4. four = "four" => "four"
+#5. print something = "nothing" => nil
+
+#Exercise 3 - Variables
+
+def multiply(a, b)
+	a * b
+end
+           
+puts multiply(3, 4)
+
+#Exercise 4 - Variables
+#Nothing. Puts is missing.
+
+#Exercise 5 - Variables
+
+def scream(words)
+	words = words + "!!!!"
+end
+
+puts scream("Yippeeee")
+
+#It returns nil
+
+#Exercise 6 - Variables
+#A method has 1 argument instead of 2.
